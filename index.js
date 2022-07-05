d0e3aea24fc05376417916d74438d82659ae14df
Laura Gonzalez <lg458@cornell.edu>
2022-07-05 13:19:46 -0700

    commit tree e879a069124f8077d933b4d7eb41b75b0355f538

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(record) {  
+    const result = record.find(checkWins); 
+    if (result != undefined) {
+      return result.year; 
+    }
+}
+
+function checkWins(obj) { 
+  return obj.result === "W";
+}
